1849e9e4e482c7f73af2f2dad4b03a87174fe8e0
GustavoBravin <gugarbravin@gmail.com>
2023-08-04 14:31:31 -0400

    kkk

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,11 @@
+import './app'
+import { Exemplo } from "./exem";
 export default function App() {
+  const listaalunos = ["Bravin, 16 anos, Mora em Batayporã","Maria, 16 anos, Mora em Nova Andradina","Gustavo, 16 anos, Mora na Favela","Marielle, 16 anos, Mora em Nova Andradina"]
     return (
       <div>
         <h1>Hello Word!</h1>
+        <Exemplo lista= {listaalunos}/>
       </div>
   
     );
---
+++ src/exem.jsx
@@ -0,0 +1,13 @@
+
+export function Exemplo({lista}) {
+return(
+    <div>
+       {lista.map(aluno=>(
+        <ul>
+             <li>{aluno}</li>
+        </ul>
+       ))}
+     
+    </div>
+);
+}
